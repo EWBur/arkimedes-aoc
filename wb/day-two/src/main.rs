916604b919918b36a8b652a542895c424d49165d
EBURSTR <elias.burstrom@volvocars.com>
2024-12-03 09:01:19 +0100

    add second star

--- wb/day-two/src/main.rs
+++ wb/day-two/src/main.rs
@@ -1,5 +1,8 @@
 use std::fs;
 
+const MAX_DIFF: u32 = 3;
+const MIN_DIFF: u32 = 1;
+
 fn read_vecs_from_file(path: &str) -> Vec<Vec<i32>> {
     let file = fs::read_to_string(path).expect("Could not read file");
     let lines: Vec<&str> = file.lines().collect();
@@ -8,9 +11,8 @@ fn read_vecs_from_file(path: &str) -> Vec<Vec<i32>> {
 
     for line in lines {
         #[cfg(feature = "dev")]
-        {
-            println!("{}", line);
-        }
+        println!("{}", line);
+
         let mut parts = line.split_whitespace();
         let mut number_vec = Vec::new();
         while let Some(p) = parts.next() {
@@ -24,33 +26,55 @@ fn read_vecs_from_file(path: &str) -> Vec<Vec<i32>> {
     vec_matrix
 }
 
-fn check_less_than_three(report: Vec<i32>) -> bool {
-    let mut three_or_less = true;
+fn has_safe_diffs(report: &Vec<i32>) -> bool {
+    let abs_diffs = report.windows(2).map(|w| w[0].abs_diff(w[1]));
+    let max_diff = abs_diffs.clone().max().unwrap();
+    let min_diff = abs_diffs.min().unwrap();
 
-    for (i, x) in report.iter().enumerate() {
-        if i == report.len() - 1 {
-            break;
-        }
-        #[cfg(feature = "dev")]
-        println!("{}", (x - report[i + 1]).abs());
-        let abs_diff = (x - report[i + 1]).abs();
-        three_or_less = three_or_less & (abs_diff <= 3) & (abs_diff != 0);
-    }
-    return three_or_less;
+    return (max_diff <= MAX_DIFF) & (min_diff >= MIN_DIFF);
 }
 
-fn is_monotonic(report: Vec<i32>) -> bool {
+fn is_monotonic(report: &Vec<i32>) -> bool {
     let decreasing = report.windows(2).all(|window| window[0] > window[1]);
     let increasing = report.windows(2).all(|window| window[0] < window[1]);
     decreasing | increasing
 }
 
-fn get_safety_vec(inp_matrix: Vec<Vec<i32>>) -> Vec<bool> {
-    let mut safe_vec = Vec::new();
+fn get_safety_vecs(inp_matrix: &Vec<Vec<i32>>) -> (Vec<bool>, Vec<bool>) {
+    let mut safe_vec_fst_star = Vec::new();
+    let mut safe_vec_snd_star = Vec::new();
     for x in inp_matrix {
-        safe_vec.push(check_less_than_three(x.clone()) & is_monotonic(x));
+        let is_safe = has_safe_diffs(&x) & is_monotonic(&x);
+        safe_vec_fst_star.push(is_safe);
+        if !is_safe {
+            let dampened_reports_matrix = create_all_problem_dampened_reports(x);
+            let safe_reports: Vec<_> = dampened_reports_matrix
+                .iter()
+                .filter(|dp| (has_safe_diffs(dp) & is_monotonic(dp)))
+                .collect();
+            safe_vec_snd_star.push(safe_reports.len() != 0);
+        }else {
+            safe_vec_snd_star.push(is_safe);
+        }
     }
-    safe_vec
+    (safe_vec_fst_star, safe_vec_snd_star)
+}
+
+fn create_all_problem_dampened_reports(report: &Vec<i32>) -> Vec<Vec<i32>> {
+    let mut dampened_reports: Vec<Vec<i32>> = Vec::new();
+    for (i, _) in report.iter().enumerate() {
+        let mut report_clone = report.clone();
+        report_clone.remove(i);
+        dampened_reports.push(report_clone);
+    }
+    #[cfg(feature = "dev")]
+    {
+        for dp in &dampened_reports {
+            println!("Dampened reports: {:?}", dp);
+        }
+        println!("End of dampened reports")
+    }
+    return dampened_reports;
 }
 
 fn main() {
@@ -61,13 +85,23 @@ fn main() {
     const FILE_NAME: &str = "input.txt";
 
     let vec_matrix = read_vecs_from_file(FILE_NAME);
-    let safety_vec = get_safety_vec(vec_matrix);
-    let mut num_safe_reports = 0;
-    for s in safety_vec {
-        if s {
-            num_safe_reports += 1;
-        }
-    }
+    let (safety_vec_fst_star, safety_vec_snd_star) = get_safety_vecs(&vec_matrix);
+    let num_safe_reports_fst_star =
+        safety_vec_fst_star
+            .iter()
+            .fold(0, |acc, x| if *x { acc + 1 } else { acc });
 
-    println!("There are {} safe reports", num_safe_reports);
+    let num_safe_reports_snd_star =
+        safety_vec_snd_star
+            .iter()
+            .fold(0, |acc, x| if *x { acc + 1 } else { acc });
+
+    println!(
+        "There are originally {} safe reports",
+        num_safe_reports_fst_star
+    );
+    println!(
+        "After problem dampening, there are {} safe reports",
+        num_safe_reports_snd_star
+    );
 }
